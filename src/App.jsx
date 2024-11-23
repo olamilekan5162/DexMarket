3326b46fee4506fa15292830afe1951910ada73f
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-11-23 08:10:23 +0100

    created more componnts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -5,6 +6,7 @@ const App = () => {
   return ( 
     <div className="container flex-col text-center h-full bg-white">
       <Navbar />
+      <Hero />
 
     </div>
    );
