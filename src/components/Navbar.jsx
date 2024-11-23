bdcc5e9858a198e7971cb923d159ee5a5fc8b19b
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-11-23 07:34:16 +0100

    almost done with the nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
+import search from '../assets/icons/icons8-search.svg'
 const Navbar = () => {
     return ( 
-        <div className="contaier h-16 flex flex-row shadow-md px-6 items-center justify-between">
+        <div className="contaier h-24 flex flex-row shadow-md px-6 items-center justify-between">
             <div className="text-blue-700 font-semibold">DEX<span className="text-black">MARKET</span></div>
-            <form action="">
-                <input type="text" name="search" id="search" placeholder="Search Product" />
+            <form action="/" className="border border-black min-w-6 rounded-md p-2 flex items-center">
+                <input className='border-none outline-none' type="text" name="search" id="search" placeholder="Search Product" />
+                <button type="submit"><img className='w-6 h-auto' src={search} alt="search-icon" /></button>
             </form>
             <ul className="flex flex-row space-x-6 items-center ">
                 <li className="hover:text-blue-700"><a href="/">Home</a></li>
                 <li className="hover:text-blue-700"><a href="#">Products</a></li>
-                <li><button className=" p-2 font-mediums border-blue-700 rounded-md hover:bg-blue-700 hover:text-white">Connect Wallet</button></li>
+                <li className="hover:text-blue-700"><a href="">Transactions</a></li>
+                <li><button className="p-1 font-medium border border-blue-700 rounded-md hover:bg-blue-700 hover:text-white">Connect Wallet</button></li>
             </ul>
 
         </div>
