1b2b953a97d9e4db3eaf65bfdd84525d369f966e
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-11-23 10:32:31 +0100

    almost done with product

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,3 @@
-import nft from '../assets/images/loko.jpg'
-
 const Products = () => {
     return ( 
         <div className="container mt-10 px-6 flex flex-col ">
@@ -7,14 +5,18 @@ const Products = () => {
                 <h1>Our Products</h1>
                 <a className="hover:text-blue-700" href="/">view all &#8594;</a>
             </div>
-            <div className="flex flex-row flex-wrap container items-center mt-6">
-                <div className="flex flex-col space-y-2 h-fit w-[200px] rounded">
-                    <div className="h-[200px] w-full bg-gray-200 rounded flex flex-col items-center space-y-2 p-6">
+            <div className="flex flex-row flex-wrap container items-center mt-6 px-2">
+                <div className="flex flex-col space-y-2 h-fit w-[200px] rounded pb-6 hover:shadow-md">
+                    <div className="h-[200px] w-full bg-gray-200 rounded flex flex-col items-center space-y-3 p-6">
                         <div className="rounded p-1 w-12 bg-red-700 text-white text-xs self-start">-30%</div>
-                        <img className="w-[100px] h-auto items-center" src={nft} alt="product" />
+                        <img className="w-[80px] h-auto items-center" src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="product" />
                     </div>
-                    <p className='font-bold'> Oracle Nft</p>
-                    <p className='text-blue-700'>$7000</p>
+                    <p className='font-bold'> Men T Shirt</p>
+                    <p>
+                        <span className='line-through text-red-700'>$-2100</span> 
+                        <span className='text-blue-700'> $7000</span>
+                    </p>
+                    <p className="font-xs">rating: 60%</p>
 
                 </div>
             </div>
