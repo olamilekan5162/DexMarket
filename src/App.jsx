db02690fcd130ec34d227bad2b25eee69df0bb37
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-11-23 06:11:07 +0100

    working on the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
+
   return ( 
-    <div className="text-red-100 text-5xl">App</div>
+    <div className="container flex-col text-center h-full p-6 bg-gray-400">
+      <Navbar />
+
+    </div>
    );
 }
  
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return ( 
+        <div className="contaier h-10 flex-row bg-red-300">
+
+        </div>
+     );
+}
+ 
+export default Navbar;
