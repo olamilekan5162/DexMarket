a7d92689a6be1be26157351f2139b5262f10c1fe
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-11-23 07:09:52 +0100

    still working on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 const App = () => {
 
   return ( 
-    <div className="container flex-col text-center h-full p-6 bg-gray-400">
+    <div className="container flex-col text-center h-full bg-white">
       <Navbar />
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,15 @@
 const Navbar = () => {
     return ( 
-        <div className="contaier h-10 flex-row bg-red-300">
+        <div className="contaier h-16 flex flex-row shadow-md px-6 items-center justify-between">
+            <div className="text-blue-700 font-semibold">DEX<span className="text-black">MARKET</span></div>
+            <form action="">
+                <input type="text" name="search" id="search" placeholder="Search Product" />
+            </form>
+            <ul className="flex flex-row space-x-6 items-center ">
+                <li className="hover:text-blue-700"><a href="/">Home</a></li>
+                <li className="hover:text-blue-700"><a href="#">Products</a></li>
+                <li><button className=" p-2 font-mediums border-blue-700 rounded-md hover:bg-blue-700 hover:text-white">Connect Wallet</button></li>
+            </ul>
 
         </div>
      );
