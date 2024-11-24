623c2bf10d148ad8611c4748c30c0f00fae5982e
olamilekan5162 <opeyemiolalekan481@gmail.com>
2024-11-24 01:04:34 +0100

    added fake hamburger

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 const Hero = () => {
     return ( 
         <div className="flex flex-col w-full mt-[120px] items-center px-16">
-            <h1 className="font-bold text-6xl sm: text-4xl">YOUR NUMBER ONE DECENTRALIZED EXCHANGE MARKET</h1>
-            <h1 className="font-bold mt-4 text-6xl text-blue-700 sm: text-4xl">DEXMARKET</h1>
+            <h1 className="font-bold text-4xl lg:text-6xl">YOUR NUMBER ONE DECENTRALIZED EXCHANGE MARKET</h1>
+            <h1 className="font-bold mt-4 text-4xl text-blue-700 lg:text-6xl">DEXMARKET</h1>
             <div className="flex flex-row space-x-5 mt-6">
                 <a className="bg-blue-700 p-2 rounded text-white hover:bg-blue-800" href="/addproduct">Add Product</a>
                 <a className="border border-blue-700 p-2 rounded hover:bg-blue-700 hover:text-white" href="#product">Shop Now &#8594;</a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import search from '../assets/icons/icons8-search.svg'
 import profile from '../assets/images/loko.jpg'
+import menu from '../assets/icons/menu.svg'
+
 const Navbar = () => {
     return ( 
         <div className="container h-24 flex flex-row shadow-md px-6 items-center justify-between fixed top-0 left-0 bg-white">
@@ -8,7 +10,7 @@ const Navbar = () => {
                 <input className='border-none outline-none' type="text" name="search" id="search" placeholder="Search Product" />
                 <button type="submit"><img className='w-6 h-auto' src={search} alt="search-icon" /></button>
             </form>
-            <ul className="flex flex-row space-x-6 items-center sm: hidden">
+            <ul className="hidden lg:flex flex-row space-x-6 items-center">
                 <li className="hover:text-blue-700"><a href="/">Home</a></li>
                 <li className="hover:text-blue-700"><a href="#">Products</a></li>
                 <li className="hover:text-blue-700"><a href="">Transactions</a></li>
@@ -16,6 +18,7 @@ const Navbar = () => {
                 <li className='w-9 h-9 rounded-full overflow-hidden'><img className='w-9 h-auto' src={profile} alt="profile" /></li>
                 <li><button className="p-1 border border-blue-700 rounded-md hover:bg-blue-700 hover:text-white">Connect Wallet</button></li>
             </ul>
+            <img className="w-9 h-auto lg:hidden " src={menu} alt="menu"/>
 
         </div>
      );
