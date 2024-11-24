df940291dd2cc4d46291d923e3ada064935093db
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-11-24 01:18:10 +0100

    finally merged my repo

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import MainLayout from "./layouts/MainLayout";
 import Homepage from "./pages/Homepage";
 
 
-const router = createBrowserRouter(createRoutesFromElements( <Route path='/' element={<MainLayout />}>
-<Route index element={<Homepage />} />
-</Route>
-  ))
+const router = createBrowserRouter(createRoutesFromElements(
+  
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<Homepage />} />
+  </Route>
+
+));
 
 const App = () => {
   return ( 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,22 @@
 const Footer = () => {
-    return ( 
-        <div className="container bg-blue-950 text-white h-[200px] grid grid-cols-4 mt-10">
-          <p>hellofffo lldjdkd</p>
-          <p>jdbdbekduxn dkeoeo</p>
-          <p>gdftyyhhvc bjppoutrtggh vftyikjh</p>
-          <p>feefhkoouy gfedfvjkpy rrdcvbkki</p>
-        </div>
-     );
+  return ( 
+      <div className="min-h-[200px] mt-20 bg-blue-950 text-white grid grid-cols-4 p-10">
+          <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, velit id. Aliquam, nesciunt ea?</p>
+          </div>
+          <div className="flex flex-col tte">
+              <p className="font-bold">Quick Links</p>
+              <a href="">Home</a>
+          </div>
+          <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, velit id. Aliquam, nesciunt ea?</p>
+          </div>
+          <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, velit id. Aliquam, nesciunt ea?</p>
+          </div>
+
+      </div>
+   );
 }
- 
+
 export default Footer;
