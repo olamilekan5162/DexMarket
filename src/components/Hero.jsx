54cda25b15c40d33e1b06099973d42241369f31d
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-12-15 02:19:19 +0100

    fixed navbar responsiveness for small screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const Hero = () => {
     return ( 
         <div className="flex flex-col w-full mt-6 items-center px-auto">
-            <h1 className="font-bold text-4xl text-center lg:text-6xl">YOUR NUMBER ONE DECENTRALIZED EXCHANGE MARKET</h1>
+            <h1 className="font-bold text-3xl text-center lg:text-6xl">YOUR NUMBER ONE DECENTRALIZED EXCHANGE MARKET</h1>
             <h1 className="font-bold mt-4 text-4xl text-center text-blue-700 lg:text-6xl">DEXMARKET</h1>
             <div className="flex flex-row gap-5 mt-6">
                 <Link className="bg-blue-700 p-2 rounded text-white hover:bg-blue-800" to="/addproduct">Add Product</Link>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,11 @@
-import searchIcon from '../assets/icons/icons8-search.svg'
 import profile from '../assets/images/loko.jpg'
-// import useConnectWallet from '../hooks/useConnectWallet'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { NavLink, Link, useNavigate } from 'react-router-dom'
+import {Menu, X, Search} from 'lucide-react'
 import { useState } from 'react'
 
 const Navbar = ( { connectWallet, account, isConnected, disconnectWallet } ) => {
-  // const { connectWallet, account, isConnected, disconnectWallet} = useConnectWallet();
-
   const [search, setSearch] = useState('')
+  const [isMenu, setIsMenu] = useState(false)
   const navigate = useNavigate()
   
   const handleConnect = async () => {
@@ -23,26 +21,65 @@ const Navbar = ( { connectWallet, account, isConnected, disconnectWallet } ) => 
 
   }
 
-  const linkStyle = ({isActive}) => isActive ? 'bg-blue-700 rounded-md text-white py-2 px-4 p1' : 'hover:text-blue-700'
+  const linkStyle = ({isActive}) => isActive ? 'bg-blue-700 rounded-md text-white py-2 px-4' : 'hover:text-blue-700'
+  const smLinkStyle = 'hover:text-blue-700'
   
     return ( 
-        <div className="w-full h-24 flex flex-row shadow-md px-6 items-center justify-between bg-white fixed top-0 left-0">
+      <>
+        <div className="w-full h-24 flex flex-row shadow-md px-6 items-center justify-between gap-2 bg-white fixed top-0 left-0">
             <div className="text-blue-700 font-semibold">DEX<span className="text-black">MARKET</span></div>
             <form  
-              className="border border-black justify-between rounded-md p-2 flex flex-row items-center"
+              className="hidden border border-black min-w-6 justify-between rounded-md lg:flex flex-row items-center"
               onSubmit={handleSearch}>
-                <input className='border-none outline-none' value={search} onChange={(e) => setSearch(e.target.value)} type="text" name="search" id="search" placeholder="Search Product" />
-                <button type="submit"><img className='w-6 h-auto' src={searchIcon} alt="search-icon" /></button>
+                <input className='border-none outline-none p-2' value={search} onChange={(e) => setSearch(e.target.value)} type="text" name="search" id="search" placeholder="Search Product" />
+                <button type="submit">
+                  <Search size={24} />
+                </button>
             </form>
-            <ul className="flex flex-row space-x-6 items-center">
+
+            <div className='lg:hidden'>
+              <Search size={24} />
+            </div>
+
+            <ul className="hidden lg:flex flex-row gap-2 items-center">
                 <li><NavLink to="/" className={linkStyle}>Home</NavLink></li>
                 <li><NavLink to="/addproduct" className={linkStyle}>Add Product</NavLink></li>
                 <li><NavLink to="/allproducts" className={linkStyle}>All Products</NavLink></li>
                 <li><NavLink to="/transactions" className={linkStyle}>Transactions</NavLink></li>
-                <li className='w-9 h-9 rounded-full overflow-hidden'><img className='w-9 h-9' src={profile} alt="profile" /></li>
-                <li><button className="p-1 border border-blue-700 rounded-md hover:bg-blue-700 hover:text-white" onClick={isConnected ? disconnectWallet : handleConnect}>{isConnected ? `${account.slice(0,5)}...${account.slice(-5)}` : 'Connect Wallet'}</button></li>
             </ul>
+
+            <div className='hidden w-9 h-9 rounded-full overflow-hidden lg:block'>
+              <img className='w-9 h-9' src={profile} alt="profile" />
+            </div>
+
+            <div>
+            <button className="p-1 border border-blue-700 rounded-md hover:bg-blue-700 hover:text-white" 
+            onClick={isConnected ? disconnectWallet : handleConnect}>{
+              isConnected ? `${account.slice(0,5)}...${account.slice(-5)}` : 'Connect Wallet'}
+            </button>
+            </div>
+
+            <div className='lg:hidden' onClick={() => setIsMenu(!isMenu)}>
+              {isMenu ? <X className='' size={24}/> :<Menu className='' size={24}/>}
+            </div>
+
         </div>
+
+        {/* menu bar for small screen */}
+
+        {isMenu &&
+        <div className='w-full h-24 mt-28 md-5 flex flex-col pl-2 gap-2 bg-white'
+        onClick={() => setIsMenu(false)}>
+        <ul className="flex flex-col gap-3">
+          <li><Link to="/" className={smLinkStyle}>Home</Link></li>
+          <li><Link to="" className={smLinkStyle}>Profile</Link></li>
+          <li><Link to="/addproduct" className={smLinkStyle}>Add Product</Link></li>
+          <li><Link to="/allproducts" className={smLinkStyle}>All Products</Link></li>
+          <li><Link to="/transactions" className={smLinkStyle}>Transactions</Link></li>
+        </ul>
+        </div>
+        }
+      </>
      );
 }
  
